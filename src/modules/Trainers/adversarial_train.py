837dce2c9d8e06ed39c6ec8792405755000fccd6
1Tanmay6 <tanmayp162004@gmail.com>
2024-08-27 10:40:22 +0530

    Completed Adversarial Training code

--- src/modules/Trainers/adversarial_train.py
+++ src/modules/Trainers/adversarial_train.py
@@ -14,8 +14,8 @@ from ..Discriminator.discriminator import Discriminator
 class AdversarialTrainer:
     def __init__(self, generator: Generator, discriminator: Discriminator) -> None:
         adv_config = get_config_files(key='adversarial_train')
-        save_images = bool(adv_config['save_images'])
-        if save_images:
+        self.save_images = bool(adv_config['save_images'])
+        if self.save_images:
             self.image_save_path = adv_config['image_save_path']
             os.makedirs(self.image_save_path, exist_ok=True)
 
@@ -45,6 +45,15 @@ class AdversarialTrainer:
             generator.parameters(), lr=self.lr_G)
         self.optimizer_D = torch.optim.Adam(
             discriminator.parameters(), lr=self.lr_D)
+
+    def __call__(self, train_loader):
+        self.train(train_loader=train_loader)
+
+    def __str__(self) -> str:
+        return "AdversarialTrainer class for training the generator and discriminator models"
+
+    def __repr__(self) -> str:
+        return "AdversarialTrainer(generator=Generator(), discriminator=Discriminator())"
 
     def train(self, train_loader: CustomDatasetGenerator):
         for epoch in range(self.num_epochs):
@@ -97,16 +106,43 @@ class AdversarialTrainer:
                         torch.nn.utils.clip_grad_norm_(
                             self.generator.parameters(), max_norm=1.0)
                         self.optimizer_G.step()
-                        print_progress(epoch, self.num_epochs, i,
-                                       100, (d_loss.item(), g_total_loss.item()), adversarial_loss=True)
+                        print_progress(epoch+1, self.num_epochs, i,
+                                       len(train_loader), (d_loss.item(), g_total_loss.item()), adversarial_loss=True)
                     self.epoch_wise_g_losses.append(g_total_loss.item())
                     self.epoch_wise_d_losses.append(d_loss.item())
 
                 except RuntimeError as e:
                     print(f"Error in batch {i}: {e}")
                     break
-            save_image(
-                gen_hr.data, f'images/gen_hr_epoch_{epoch}.png', normalize=True)
+            if self.save_images:
+                save_image(
+                    gen_hr.data, f'{self.image_save_path}/gen_hr_epoch_{epoch}.png', normalize=True)
+                save_image(
+                    imgs_lr, f'{self.image_save_path}/gen_lr_epoch_{epoch}.png', normalize=True)
+
+    def get_models(self):
+        return self.generator, self.discriminator
+
+    def get_losses(self):
+        return self.gen_losses, self.dis_losses, self.real_losses, self.fake_losses, self.g_losses, self.content_losses, self.epoch_wise_g_losses, self.epoch_wise_d_losses
+
+    def get_optimizers(self):
+        return self.optimizer_G, self.optimizer_D
+
+    def get_criteria(self):
+        return self.adversarial_loss, self.content_loss
+
+    def save_models(self, path):
+        torch.save(self.generator.state_dict(),
+                   os.path.join(path, 'generator.pth'))
+        torch.save(self.discriminator.state_dict(),
+                   os.path.join(path, 'discriminator.pth'))
+
+    def save_models_script(self, path):
+        model_scripted = torch.jit.script(self.generator)
+        model_scripted.save(os.path.join(path, 'generator.pt'))
+        model_scripted = torch.jit.script(self.discriminator)
+        model_scripted.save(os.path.join(path, 'discriminator.pt'))
 
 
 if __name__ == '__main__':
--- src/modules/Utils/__init__.py
+++ src/modules/Utils/__init__.py
@@ -1,7 +1,24 @@
 import os
 import sys
+import torch
 import configparser
 from colorama import Fore, Style
+
+CYAN = Fore.CYAN
+GRAY = Fore.LIGHTBLACK_EX
+GREEN = Fore.GREEN
+WHITE = Fore.WHITE
+RED = Fore.RED
+BLUE = Fore.LIGHTBLUE_EX
+
+
+def load_model(model_path, model=None, type='script'):
+    if type == 'script':
+        model_scripted = torch.jit.load(model_path)
+        return model_scripted
+    else:
+        model.load_state_dict(torch.load(model_path))
+        return model
 
 
 def get_config_files(key: str):
@@ -13,20 +30,21 @@ def get_config_files(key: str):
 
 def print_progress(epoch, num_epochs, batch, num_batches, loss, bar_length=30, adversarial_loss=False):
     progress = (batch + 1) / num_batches
-    progress_bar = int(progress * bar_length)
+    progress_bar = int(bar_length * progress)
 
+    sys.stdout.write(f"{CYAN}{Style.BRIGHT}Epoch [{epoch}/{num_epochs}] |")
     sys.stdout.write(
-        f"{Fore.CYAN}{Style.BRIGHT}Epoch [{epoch}/{num_epochs}] |")
-    sys.stdout.write(f"{Fore.YELLOW}{
-                     '█' * progress_bar}{Fore.WHITE}{'-' * (bar_length - progress_bar)}")
-    sys.stdout.write(f"| {Fore.GREEN}{int(progress * 100)}% {Style.RESET_ALL}")
+        f"{GRAY}{'█' * progress_bar}{WHITE}{' ' * (bar_length - progress_bar)}")
+    sys.stdout.write(f"| {GREEN}{int(progress * 100)}%{Style.RESET_ALL}")
+
     if adversarial_loss:
         sys.stdout.write(
-            f" [Batch {batch + 1}/{num_batches}] [D loss: {Fore.RED}{loss[0]:.4f}{Style.RESET_ALL}] [G loss: {Fore.BLUE}{loss[1]:.4f}{Style.RESET_ALL}]\r")
+            f" [Batch {batch + 1}/{num_batches}] [D loss: {RED}{loss[0]:.4f}{Style.RESET_ALL}] [G loss: {BLUE}{loss[1]:.4f}{Style.RESET_ALL}]\r")
     else:
         sys.stdout.write(
-            f" [Batch {batch + 1}/{num_batches}] [Loss: {Fore.RED}{loss:.4f}{Style.RESET_ALL}]\r")
+            f" [Batch {batch + 1}/{num_batches}] [Loss: {RED}{loss:.4f}{Style.RESET_ALL}]\r")
+
     sys.stdout.flush()
 
 
-__all__ = ['print_progress', 'get_config_files']
+__all__ = ['print_progress', 'get_config_files', 'load_model']
