8ccb0bf2df68c5eb003f027aeb963fb5c958b3ca
1Tanmay6 <tanmayp162004@gmail.com>
2024-09-03 15:54:28 +0530

    Changed the package name

--- src/main.py
+++ src/main.py
@@ -1,5 +1,5 @@
-from modules.Utils import get_config_files
-from modules.Generator.generator import Generator
+from SRGAN.Utils import get_config_files
+from SRGAN.Generator.generator import Generator
 
 if __name__ == '__main__':
     g = Generator()
